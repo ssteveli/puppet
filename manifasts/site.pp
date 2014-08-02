a29fcca5da041d88ad7869e4c56f06d57c55dfc8
Scott Stevelinck <scott@stevelinck.com>
2014-08-02 07:43:15 -0600

    fw work

--- manifasts/site.pp
+++ manifasts/site.pp
@@ -1,3 +1,15 @@
+resources { "firewall":
+	purge => true,
+}
+
+Firewall {
+	before => Class['my_fw::post'],
+	require => Class['my_fw::pre'],
+}
+
+class { ['myfw::pre', 'my_fw::post']: }
+class { 'firewall': }
+
 node default {
 	include sudo::install
 	include sudo::sudoers
