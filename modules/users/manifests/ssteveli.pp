f9056cfcb9323165b4f18220bb609fe1384b21b6
Scott Stevelinck <scott@stevelinck.com>
2014-08-02 16:35:42 -0600

    added my macbook key

--- modules/users/manifests/ssteveli.pp
+++ modules/users/manifests/ssteveli.pp
@@ -25,7 +25,7 @@ class users::ssteveli {
 		mode => '0700',
 		require => File['/home/ssteveli'],
 	}
-	
+
 	file { '/etc/sudoers.d/ssteveli':
 		ensure => 'file',
 		content => 'ssteveli ALL=(ALL) NOPASSWD: ALL',
@@ -34,11 +34,20 @@ class users::ssteveli {
 		group => 'root',
 	}
 
-	ssh_authorized_key { 'ssteveli-ssh-key':
+	ssh_authorized_key { 'ssteveli-ssh-key-imac':
 		ensure => 'present',
 		type => 'ssh-rsa',
 		key => 'AAAAB3NzaC1yc2EAAAABIwAAAQEA4gBGNQYvtf1Nq+UZAtRkq+2ckTyETPQhlsCRVkmhcWMOznfOz1ozsDAXfkdQPbFEgGPjEGYOEbTSUnoTF6IxdE6s91trPTIQkXqQzg0LwEZtoPbyaDxopUEXPG5nvlpB33wcGEpDhWp7pwOeUwMyLP3lGX/IT2GhhwXtr50PPfxKY1hqPQW1KAuslLPE939V8iyuJsY/+tpibBYkkWTJK7m9LS+3NC0SMNganhkVg/IBuD9FwefOepoNJTo39NWyloUufSQKxDJVo0QXWu86q/BbyeDmtIaH8a5TLJyWCjOK1tAWNz8BhrviVxORhQdC17qjx6MxsdZkf3bM1yOHmQ==',
 		user => 'ssteveli',
 		require => File['/home/ssteveli/.ssh'],
 	}
+
+	ssh_authorized_key { 'ssteveli-ssh-key-macbookair':
+		ensure => 'present',
+		type => 'ssh-rsa',
+		key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDWxWY2OUadNjOiUL4YWMX+ZOvItNIo8zcMAfgXqRS1+vyClzIRZFDkTzgdG7jdrD2rIpa/Il70iW64PzP9gx65kVCPGsGmP6ay1MbMEP/y8toUakskT9ZHBykD0FFZ0KS1i2J0S6SKVLg0UXO8ysUtructyoBArvqE95X355BioLDJYmpuJpbnpJswPMuJA4DW+WmYjfC9wHjMjtpomwGM7bCO3hkjdp+DCjSzyHrC9dod+3MqYqk9yENwfHKS4BE1cYzDW8ixcYmqYzXwK1BHbiIlqYpSJut+q9QmGFqLxukPzZrhJHWxInvkVE3vgURC7C6EH+Z8ydoGvLYQ//Qb',
+		user => 'ssteveli',
+		require => File['/home/ssteveli/.ssh'],
+	}
+
 }
