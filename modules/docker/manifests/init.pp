3d530201668374ae0a54a47b70e392878c696bbb
Scott Stevelinck <scott@stevelinck.com>
2014-08-09 07:06:50 -0600

    adding fw rules for docker

--- modules/docker/manifests/init.pp
+++ modules/docker/manifests/init.pp
@@ -8,4 +8,29 @@ class docker {
 		ensure => 'running',
 		require => Package['docker'],
 	}
+	
+	firewall { '300 allow docker0 connections':
+		chain => 'FORWARD',
+		action => 'ACCEPT',
+		outiface => 'docker0',
+		ctstate => ['RELATED','ESTABLISHED'],
+		proto => 'all',
+	}
+	
+	firewall { '301 allow docker0 traffic out':
+		chain => 'FORWARD',
+		action => 'ACCEPT',
+		proto => 'all',
+		iniface => 'docker0',
+		outiface => 'eth0',
+	}
+	
+	firewall { '302 allow docker0 traffic to docker0':
+		chain => 'FORWARD',
+		action => 'ACCEPT',
+		proto => 'all',
+		iniface => 'docker0',
+		outiface => 'docker0',
+	}
+		
 }
